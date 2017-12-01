b00358d69dfcbe57372bff34b02a1ba2e7a1837c
Nora White <noraw@telus.net>
2017-11-30 22:14:58 -0700

    Totem and warrior comments

--- The Kingdom/The Kingdom/TotemFriendly.h
+++ The Kingdom/The Kingdom/TotemFriendly.h
@@ -2,7 +2,7 @@
 // CS 2720: GeneralFailure
 //
 /// \author Taylor Doud, Matthew Rose
-/// \date Oct 26, 2017
+/// \date Nov 26, 2017
 ///
 ///
 /// The TotemFriendly class is a concrete derived class, inherits from Totem.
@@ -20,11 +20,19 @@ class TotemFriendly : public Totem
 {
 public:
     
+	/// Creates the TotemFriendly object and initializes it.
+	/// \param[in] data The structure of game data.
 	TotemFriendly(GameDataRef data);
     
-    /// Creates the TotemFriendly object and initializes it.
-    /// \param[in] id ID of the card.
-    /// \param[in] amount The value to be used by the effect.
+	/// Creates the TotemFriendly object and initializes it.
+	/// \param[in] data The structure of game data.
+	/// \param[in] name The name of the card.
+	/// \param[in] description The description of the card.
+	/// \param[in] cost The amount of action points required to play the card.
+	/// \param[in] ability The char that is related to which type of ability the card does.
+	/// \param[in] taunt A boolean of whether a taunt is active on this card.
+	/// \param[in] health The amount of health that the card has.
+	/// \param[in] amount The value to be used by the effect.
     TotemFriendly(GameDataRef data, std::string name, std::string description, int cost, char ability, bool taunt, int health, int amount);
     
     /// Destroys the TotemFriendly object.
@@ -34,16 +42,25 @@ public:
     /// \param[in] board The current game board.
     void effect(Board* board);
 
+	/// Reading in the card data from a packet.
+	/// \param[in] The input packet.
 	void read(sf::Packet & is);
+
+	/// Writing the card data to the packet.
+	/// \param[in] os The outgoing packet.
+	/// \return The completed packet
 	sf::Packet write(sf::Packet &os) override;
 
-	/// Gets the Minions damageValue.
-	/// \return The Minions damageValue.
+	/// Gets the TotemPlayer's damageValue.
+	/// \return The TotemPlayer's damageValue.
 	sf::Int8 getDamage() { return -1; }
 
-	/// Sets the Minions damageValue.
+	/// Sets the TotemPlayer's damageValue.
+	/// \param[in] damagealue The amount of damage that the Totem delivers
 	void setDamage(int damagealue) {}
 
+	/// Gets the ID of the card
+	/// \return The ID of the card
 	sf::Int8 getId() override
 	{
 		return TOTEM_FRIENDLY_CARD;
